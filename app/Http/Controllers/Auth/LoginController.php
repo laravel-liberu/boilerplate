b929355c2a04a8c991a790a8cc53ad0727fe308d
Sohel Rana <sohel@eskimi.com>
2023-10-05 00:29:52 +0600

    Registration done

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Auth;
 
+use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Models\UserSocial;
 use Illuminate\Http\Request;
@@ -34,11 +35,11 @@ class LoginController extends Controller
     // {
     //     return Socialite::driver($service)->redirect();
     // }
-    
+
     //validate if the the provider is expected (facebook, google, github) 
     protected function validateProvider($provider)
     {
-        if (! in_array($provider, ['facebook', 'google', 'github'])) {
+        if (!in_array($provider, ['facebook', 'google', 'github'])) {
             return response()->json(['error' => 'Please login using facebook or google or github'], 422);
         }
     }
@@ -46,14 +47,14 @@ class LoginController extends Controller
     //check if the user has socila linked
     public function needsToCreateSocial(User $user, $service)
     {
-        return ! $user->hasSocialLinked($service);
+        return !$user->hasSocialLinked($service);
     }
 
 
     public function redirectToProvider($provider)
     {
         $validated = $this->validateProvider($provider);
-        if (! is_null($validated)) {
+        if (!is_null($validated)) {
             return $validated;
         }
 
@@ -62,11 +63,11 @@ class LoginController extends Controller
 
     public function providerCallback($provider)
     {
-        
-        try{
+
+        try {
             $user = Socialite::driver($provider)->stateless()->user();
         } catch (Exception) {
-            return redirect(config('settings.clientBaseUrl').'/social-callback?token=&status=false&message=Invalid credentials provided!');
+            return redirect(config('settings.clientBaseUrl') . '/social-callback?token=&status=false&message=Invalid credentials provided!');
         }
 
         $curUser = User::where('email', $user->getEmail())->first();
@@ -80,11 +81,11 @@ class LoginController extends Controller
                 ]);
             }
         } catch (Exception) {
-            return redirect(config('settings.clientBaseUrl').'/social-callback?token=&status=false&message=Something went wrong!');
+            return redirect(config('settings.clientBaseUrl') . '/social-callback?token=&status=false&message=Something went wrong!');
         }
 
         //if the user exists then login:
-            
+
         try {
             if ($this->needsToCreateSocial($curUser, $provider)) {
                 UserSocial::create([
@@ -94,17 +95,17 @@ class LoginController extends Controller
                 ]);
             }
         } catch (Exception) {
-            return redirect(config('settings.clientBaseUrl').'/social-callback?token=&status=false&message=Something went wrong!');
+            return redirect(config('settings.clientBaseUrl') . '/social-callback?token=&status=false&message=Something went wrong!');
         }
-            
+
         //check if the user is not blocked or something
         if ($this->loggableSocialUser($curUser)) {
             Auth::guard('web')->login($curUser, true);
 
-            return redirect(config('settings.clientBaseUrl').'/social-callback?token='.csrf_token().'&status=success&message=success');
+            return redirect(config('settings.clientBaseUrl') . '/social-callback?token=' . csrf_token() . '&status=success&message=success');
         }
 
-            return redirect(config('settings.clientBaseUrl').'/social-callback?token=&status=false&message=Something went wrong while we processing the login. Please try again!');
+        return redirect(config('settings.clientBaseUrl') . '/social-callback?token=&status=false&message=Something went wrong while we processing the login. Please try again!');
 
     }
 
@@ -112,11 +113,11 @@ class LoginController extends Controller
     {
         $user = User::whereEmail($request->input('email'))->first();
 
-        if (! optional($user)->currentPasswordIs($request->input('password'))) {
+        if (!optional($user)->currentPasswordIs($request->input('password'))) {
             return;
         }
 
-        if (! $user->email) {
+        if (!$user->email) {
             throw ValidationException::withMessages([
                 'email' => 'Email does not exist.',
             ]);
@@ -133,7 +134,7 @@ class LoginController extends Controller
             ]);
         }
 
-        if (! App::runningUnitTests()) {
+        if (!App::runningUnitTests()) {
             $company = $user->person->company();
             //            \Log::debug('Login----------------------'.$company);
             $tenant = false;
@@ -142,20 +143,20 @@ class LoginController extends Controller
             }
             // set company id as default
             $main_company = $user->person->company();
-            if ($main_company !== null && ! $user->isAdmin()) {
+            if ($main_company !== null && !$user->isAdmin()) {
                 $c_id = $main_company->id;
             }
-            if (! $user->isAdmin()) {
+            if (!$user->isAdmin()) {
                 $tenants = Tenant::find($main_company->id);
             }
             if ($user->isAdmin()) {
                 $tenants = null;
             }
-            if ($main_company === null && ! $user->isAdmin()) {
+            if ($main_company === null && !$user->isAdmin()) {
                 //   if (($main_company == null||$tenants=='') && ! $user->isAdmin()) {
                 //   if ($main_company == null) {
                 $this->create_company($user);
-            } elseif ($tenants && ! $user->isAdmin()) {
+            } elseif ($tenants && !$user->isAdmin()) {
                 //                    $c = DB::connection('tenantdb',$tenants->tenancy_db_name)->table('users')->count();
                 $company = \App\Models\Company::find($main_company->id);
                 //                    \Log::debug('Database----------------------'.$main_company->id);
