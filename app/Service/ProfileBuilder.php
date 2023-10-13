93c3cea23aebedfa033d7e08193a676b64f701f1
Curtis Delicata <curtis.delicata@outlook.com>
2023-10-13 14:14:06 +0000

    Add storage

--- app/Service/ProfileBuilder.php
+++ app/Service/ProfileBuilder.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Services;
+namespace App\Service;
 
 use App\Models\User;
 use Carbon\Carbon;
--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel Enso'),
+    'name' => env('APP_NAME', 'Laravel Liberu'),
 
     /*
     |--------------------------------------------------------------------------
@@ -67,7 +67,7 @@ return [
     |
     */
 
-    'timezone' => 'Europe/Bucharest',
+    'timezone' => 'Europe/London',
 
     /*
     |--------------------------------------------------------------------------
